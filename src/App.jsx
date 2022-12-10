13b341e26dae761d3811c0f6361f476497239b25
Gjenius20 <dafadhiyaulhaq972@gmail.com>
2022-12-10 23:47:34 +0700

    10/12/2022

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 import ThreeModels from "./Components/ThreeModels";
 import "./index.css";
 
 const App = () => {
   return (
-    <header className="w-screen h-screen">
+    <header className="w-screen h-screen overflow-hidden">
+      <Navbar />
       <ThreeModels />
     </header>
   );
